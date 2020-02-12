b6ab5bfdfd0fb595632241d5dd4fafc312d8b2c3
Derrick Song <derricksong@users.noreply.github.com>
2020-02-12 11:13:22 -0800

    Create wrps.py

---
+++ wrps.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import cherrypy
+class rps(object):
+
+    cherrypy.server.socket_host = '0.0.0.0'
+    players = {}
+    games = []
+
+    @cherrypy.expose
+    def index(self):
+        return "What is your name?<br /><form action=ready><input type='text' name='name' value='...'/><input type='submit' value='go'/></form>"
+
+    @cherrypy.expose
+    def ready(self,name='nothing'):
+        output = "You are %s. No? <a href='/'>back</a><br /><br />"%name
+        cherrypy.session['name'] = name
+        output += "Who do you want to play?<br />"
+        output += "<form method=start><ul>"
+        for player in self.players.keys():
+            if self.players[player] == 'ready':
+                output += "<input type='radio' name='player' value='%s'><li>%s</li>"%(player,player)
+        output += "<input type='submit' value='Go'/></form>"
+        self.players[name] = 'ready'
+        return output
+
+    def start(self,player):
+        moves = []
+        pass
+
+
+
+
+cherrypy.quickstart(rps())
